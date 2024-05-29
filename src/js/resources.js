9536b784d6ca3b468a9c991e4864a6f4aab8fa7c
Geert van der Velden <1079252@hr.nl>
2024-05-29 19:45:32 +0200

    fingers crossed

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,23 +3,23 @@ import { TiledResource } from '@excaliburjs/plugin-tiled';
 
 // Voeg hier jouw eigen resources toe
 const Resources = {
-    Playerright: new ImageSource('public/images/ship13.png'),
-    Playerdown: new ImageSource('public/images/ship9.png'),
-    Playerdownright: new ImageSource('public/images/ship11.png'),
-    Playerdownleft: new ImageSource('public/images/ship7.png'),
-    Playerleft: new ImageSource('public/images/ship5.png'),
-    playerupleft: new ImageSource('public/images/ship3.png'),
-    playerup: new ImageSource('public/images/ship1.png'),
-    playerupright: new ImageSource('public/images/ship15.png'),
-    tilemapje: new TiledResource('public/images/tilemap1.tmx'), 
+    Playerright: new ImageSource('images/ship13.png'),
+    Playerdown: new ImageSource('images/ship9.png'),
+    Playerdownright: new ImageSource('images/ship11.png'),
+    Playerdownleft: new ImageSource('images/ship7.png'),
+    Playerleft: new ImageSource('images/ship5.png'),
+    playerupleft: new ImageSource('images/ship3.png'),
+    playerup: new ImageSource('images/ship1.png'),
+    playerupright: new ImageSource('images/ship15.png'),
+    tilemapje: new TiledResource('images/tilemap1.tmx'), 
 
-    badguy: new ImageSource('public/images/badguyship.png'),
-    fireball: new ImageSource('public/images/fireball.png'),
-    kanon: new ImageSource('public/images/kanon.png'),
-    explosion: new ImageSource('public/images/explosion.png'),
-    hit: new ImageSource('public/images/hit.png'),
-    canonball: new ImageSource('public/images/canonball.png'),
-    backgroundMusic: new Sound('public/music/Piratetheme.mp3')
+    badguy: new ImageSource('images/badguyship.png'),
+    fireball: new ImageSource('images/fireball.png'),
+    kanon: new ImageSource('images/kanon.png'),
+    explosion: new ImageSource('images/explosion.png'),
+    hit: new ImageSource('images/hit.png'),
+    canonball: new ImageSource('images/canonball.png'),
+    backgroundMusic: new Sound('music/Piratetheme.mp3')
     
 };
 
